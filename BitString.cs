d9797679539fa6575b5d8213ad6a316111e83beb
Diak Maksym <55993687+FlaxyDev@users.noreply.github.com>
2023-09-23 02:02:18 +0300

    Add files via upload

---
+++ BitString.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    public class BitString
+    {
+        private StringBuilder stringBuilder;
+
+        public event EventHandler FilledWithZeros;
+
+        public BitString()
+        {
+            stringBuilder = new StringBuilder();
+        }
+
+        // Додавання бітової послідовності до стрічки
+        public void AddBits(string bits)
+        {
+            foreach (char bit in bits)
+            {
+                if (bit != '0' && bit != '1')
+                {
+                    throw new ArgumentException("Invalid bit: " + bit);
+                }
+
+                stringBuilder.Append(bit);
+            }
+        }
+
+        // Повертає біт за індексом
+        public bool GetBit(int index)
+        {
+            if (index < 0 || index >= stringBuilder.Length)
+            {
+                throw new IndexOutOfRangeException("Index is out of range");
+            }
+
+            return stringBuilder[index] == '1';
+        }
+
+        // Повертає кількість бітів у стрічці
+        public int Length()
+        {
+            return stringBuilder.Length;
+        }
+
+        // Повертає рядок, що представляє бітову стрічку
+        public override string ToString()
+        {
+            return stringBuilder.ToString();
+        }
+
+        public void CheckString()
+        {
+            if (stringBuilder.ToString().All(bit => bit == '0'))
+            {
+                // Виникнення події, стрічка заповннена нулями
+                OnFilledWithZeros(EventArgs.Empty);
+            }
+        }
+
+        // Заміна символу за індексом на вказаний символ
+        public void ReplaceChar(int index, char replacement)
+        {
+            if (index < 0 || index >= stringBuilder.Length)
+            {
+                throw new IndexOutOfRangeException("Index is out of range");
+            }
+
+            stringBuilder[index] = replacement;
+        }
+
+        protected virtual void OnFilledWithZeros(EventArgs e)
+        {
+            FilledWithZeros?.Invoke(this, e);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BitString bitString = new BitString();
+            bitString.AddBits("0000");
+            bitString.FilledWithZeros += String_FilledWithZeros;
+
+            bitString.CheckString();
+
+            Console.WriteLine(bitString.ToString());
+        }
+        private static void String_FilledWithZeros(object sender, EventArgs e)
+        {
+            BitString bitString = (BitString)sender;
+            Console.WriteLine("BitString is filled with zeros.");
+
+            bitString.ReplaceChar(bitString.Length() - 1, '1');
+        }
+    }
+}
